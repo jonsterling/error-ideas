6380d9a6c3a772d45fd37e4eda0cabe3dd742494
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-14 22:24:35 -0700

    Use a pointed monoid to accumulate traces

--- src/Control/Monad/Trace/ErrorTrace.hs
+++ src/Control/Monad/Trace/ErrorTrace.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UnicodeSyntax #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Control.Monad.Trace.ErrorTrace
 ( ErrorTrace(..)
@@ -11,28 +13,31 @@ module Control.Monad.Trace.ErrorTrace
 import Control.Applicative
 import qualified Data.Foldable as F
 import Data.Monoid
+-- import Data.Pointed
 import Data.Profunctor
 import Data.Sequence as S
 import Data.List
 
 -- | A datatype containing an error and its provenience(s).
 --
-data ErrorTrace t e
+data ErrorTrace t e mt
   = ErrorTrace
   { _etError ∷ !e -- ^ The error
-  , _etTrace ∷ ![Seq t] -- ^ The list of traces (for each path tried)
+  , _etTrace ∷ mt (Seq t) -- ^ The pointed monoid accumulating path traces
   }
 
-instance Monoid e ⇒ Monoid (ErrorTrace t e) where
+instance (Monoid e, Monoid (mt (Seq t))) ⇒ Monoid (ErrorTrace t e mt) where
   mempty = ErrorTrace mempty mempty
   mappend (ErrorTrace e tr) (ErrorTrace e' tr') = ErrorTrace (e <> e') (tr <> tr')
 
-instance (Show t, Show e) ⇒ Show (ErrorTrace t e) where
-  showsPrec p ErrorTrace{..} =
-    showParen (p > 10) $
-      foldr (.) id (intersperse (" ∥ "++) $ (foldr (.) id . intersperse ('.':) . fmap shows . F.toList) <$> _etTrace)
-      . (" ⇑ " ++)
-      . shows _etError
+deriving instance (Show e, Show (mt (Seq t))) ⇒ Show (ErrorTrace t e mt)
+
+-- instance (Show t, Show e) ⇒ Show (ErrorTrace t e mt) where
+--   showsPrec p ErrorTrace{..} =
+--     showParen (p > 10) $
+--       foldr (.) id (intersperse (" ∥ "++) $ (foldr (.) id . intersperse ('.':) . fmap shows . F.toList) <$> _etTrace)
+--       . (" ⇑ " ++)
+--       . shows _etError
 
 -- | An isomorphism @'ErrorTrace' t e ≅ (e, 'Seq' t)@.
 --
@@ -40,8 +45,8 @@ _ErrorTrace
   ∷ ( Choice p
     , Functor f
     )
-  ⇒ p (ErrorTrace t e) (f (ErrorTrace t' e'))
-  → p (e, [Seq t]) (f (e', [Seq t']))
+  ⇒ p (ErrorTrace t e mt) (f (ErrorTrace t' e' mt))
+  → p (e, mt (Seq t)) (f (e', mt (Seq t')))
 _ErrorTrace =
   dimap (uncurry ErrorTrace)
   . fmap $ \ErrorTrace{..} → (_etError, _etTrace)
@@ -51,8 +56,8 @@ _ErrorTrace =
 etError
   ∷ Functor f
   ⇒ (e → f e')
-  → ErrorTrace t e
-  → f (ErrorTrace t e')
+  → ErrorTrace t e mt
+  → f (ErrorTrace t e' mt)
 etError inj ErrorTrace{..} =
   flip ErrorTrace _etTrace
     <$> inj _etError
@@ -61,10 +66,9 @@ etError inj ErrorTrace{..} =
 --
 etTrace
   ∷ Functor f
-  ⇒ ([Seq t] → f [Seq t'])
-  → ErrorTrace t e
-  → f (ErrorTrace t' e)
+  ⇒ (mt (Seq t) → f (mt (Seq t')))
+  → ErrorTrace t e mt
+  → f (ErrorTrace t' e mt)
 etTrace inj ErrorTrace{..} =
   ErrorTrace _etError
     <$> inj _etTrace
-
--- src/Control/Monad/Trans/FreeTrace.hs
+++ src/Control/Monad/Trans/FreeTrace.hs
@@ -27,54 +27,57 @@ import Control.Monad.Trans
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.Either
 import Control.Monad.Trans.State.Strict
+import Data.Pointed
 import Data.Sequence as S hiding (singleton)
 
 import Control.Monad.Reader
 
-data TraceF t e m α where
-  Catch ∷ TraceT t e m α → (e → TraceT t e m α) → TraceF t e m α
-  Read ∷ TraceF t e m (Seq t)
-  Scope ∷ t → TraceT t e m α → TraceF t e m α
-  Throw ∷ e → TraceF t e m α
+data TraceF t e mt m α where
+  Catch ∷ TraceT t e mt m α → (e → TraceT t e mt m α) → TraceF t e mt m α
+  Read ∷ TraceF t e mt m (Seq t)
+  Scope ∷ t → TraceT t e mt m α → TraceF t e mt m α
+  Throw ∷ e → TraceF t e mt m α
 
-newtype TraceT t e m α = TraceT { _traceT ∷ ProgramT (TraceF t e m) m α }
+newtype TraceT t e (mt :: * -> *) m α = TraceT { _traceT ∷ ProgramT (TraceF t e mt m) m α }
 
-deriving instance Monad m ⇒ Applicative (TraceT t e m)
-deriving instance Monad m ⇒ Functor (TraceT t e m)
-deriving instance Monad m ⇒ Monad (TraceT t e m)
-deriving instance MonadIO m ⇒ MonadIO (TraceT t e m)
-deriving instance MonadReader r m ⇒ MonadReader r (TraceT t e m)
-deriving instance MonadState s m ⇒ MonadState s (TraceT t e m)
+deriving instance Monad m ⇒ Applicative (TraceT t e mt m)
+deriving instance Monad m ⇒ Functor (TraceT t e mt m)
+deriving instance Monad m ⇒ Monad (TraceT t e mt m)
+deriving instance MonadIO m ⇒ MonadIO (TraceT t e mt m)
+deriving instance MonadReader r m ⇒ MonadReader r (TraceT t e mt m)
+deriving instance MonadState s m ⇒ MonadState s (TraceT t e mt m)
 
-instance Monad m ⇒ MonadError e (TraceT t e m) where
+instance Monad m ⇒ MonadError e (TraceT t e mt m) where
   catchError m = TraceT . singleton . Catch m
   throwError = TraceT . singleton . Throw
 
-instance MonadTrans (TraceT t e) where
+instance MonadTrans (TraceT t e mt) where
   lift = TraceT . lift
 
-instance Monad m ⇒ MonadTrace t (TraceT t e m) where
+instance Monad m ⇒ MonadTrace t (TraceT t e mt m) where
   traceScope t = TraceT . singleton . Scope t
   readTrace = TraceT $ singleton $ Read
 
 runTraceT
   ∷ ( Functor m
     , Monad m
+    , Pointed mt
     )
-  ⇒ TraceT t e m α
-  → EitherT (ErrorTrace t e) m α
+  ⇒ TraceT t e mt m α
+  → EitherT (ErrorTrace t e mt) m α
 runTraceT = run S.empty . _traceT
 
 run
-  ∷ Monad m
+  ∷ ( Monad m
+    , Pointed mt
+    )
   ⇒ Seq t
-  → ProgramT (TraceF t e m) m α
-  → EitherT (ErrorTrace t e) m α
+  → ProgramT (TraceF t e mt m) m α
+  → EitherT (ErrorTrace t e mt) m α
 run ts m = EitherT $ runEitherT . go ts =<< viewT m
   where
-    go ∷ Monad m ⇒ Seq t → ProgramViewT (TraceF t e m) m α → EitherT (ErrorTrace t e) m α
     go ts (Catch m h :>>= k) = run ts (_traceT m) `catchError` (run ts . _traceT . h . _etError) >>= run ts . k
     go ts (Read :>>= k) = right ts >>= run ts . k
     go ts (Scope t m :>>= k) = run (ts |> t) (_traceT m) >>= run ts . k
-    go ts (Throw e :>>= k) = left (ErrorTrace e [ts]) >>= run ts . k
+    go ts (Throw e :>>= k) = left (ErrorTrace e (point ts)) >>= run ts . k
     go ts (Return a) = right a
